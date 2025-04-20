827bfd6f47979ed45355f7d0ced26ea1375d853b
yuxin wu <yuwu@student.hive.fi>
2025-04-20 14:45:05 +0300

    200525_midday

---
+++ ft_itoa.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuwu <yuwu@student.hive.fi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/20 13:50:31 by yuwu              #+#    #+#             */
+/*   Updated: 2025/04/20 14:31:01 by yuwu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2);
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	multi;
+
+	multi = 1;
+	if (power < 0)
+		return (0);
+	if ((nb == 0) && (power == 0))
+		return (1);
+	while (power >= 1)
+	{
+		multi *= nb;
+		power--;
+	}
+	return (multi);
+}
+
+int	ft_count_int(int n)
+{
+	int	num;
+
+	num = 1;
+	while (n / 10 >= 1)
+	{
+		n /= 10;
+		num++;
+	}
+	return (num);
+}
+
+char	*ft_is_special(int n)
+{
+	char	*special;
+
+	if (n != -2147483648)
+		return (NULL);
+	special = malloc(sizeof(char) * 12);
+	special = "-2147483648";
+	special[12] = '\0';
+	return (special);
+}
+
+char	*ft_itoa_positive(int n)
+{
+	int		i;
+	int		size;
+	char	*result;
+	int		power;
+
+	i = 0;
+	size = ft_count_int(n);
+	result = malloc(sizeof(char) * (n + 1));
+	while (i < size)
+	{
+		power = ft_iterative_power(10, (size - 1));
+		result[i] = n / power;
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*minus;
+	
+	minus = "-";
+	if (n == -2147483648)
+		return (ft_is_special(n));
+	if (n < 0)
+		return (ft_strjoin(minus, ft_itoa_positive(-n)));
+	return (ft_itoa_positive(n));
+}
---
+++ ft_split.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yuwu <yuwu@student.hive.fi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/20 13:24:34 by yuwu              #+#    #+#             */
+/*   Updated: 2025/04/20 13:47:22 by yuwu             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+size_t		ft_strlen(const char *str);
+
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+
+size_t	ft_count_c(char const *s, char c)
+{
+	size_t	c_count;
+	
+	c_count = 0;
+	while (*s)
+	{
+		if ((*s) == c)
+			c_count++;
+		s++;
+	}
+	return c_count;
+}
+//It counts how many c:s is going to be in the str.
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	len;
+	size_t	num_of_output;
+	size_t	output;
+	size_t	x; //length of each string
+
+	len = ft_strlen(s);
+	num_of_output = ft_count_c(s, c) + 1;
+	output = 0;
+	while (output < num_of_output)
+	{
+		x = 0;
+		while (s[x] != c)
+		{
+			des_output[x] =  s[x]
+			x++;
+		}
+		return des_output			
+		output++;
+	}
+}
+
+
+/*
+Parameters s: 	The string to be split.
+c: 				The delimiter character.
+
+Return value 
+The array of new strings resulting from the split.
+NULL if the allocation fails.
+
+External functs. malloc, free
+Description 
+Allocates memory (using malloc(3)) 
+and returns an array of strings obtained by splitting ’s’ using
+the character ’c’ as a delimiter. 
+The array must end with a NULL pointer.
+*/
+

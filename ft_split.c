e3871e33da14e1ee09aa93fe41aae650f0dff908
yuxin wu <yuwu@student.hive.fi>
2025-04-25 19:47:54 +0300

    Split_still_not_working

--- ft_split.c
+++ ft_split.c
@@ -32,43 +32,43 @@ size_t	ft_count_total_strs(char const *s, char c)
 	return (strs_count);
 }
 
-char	*ft_showlength_eachstr(char const *s, char c)
+size_t	*ft_showlength_eachstr(char const *s, char c)
 {
-	int		i;
+	size_t		i;
 	size_t	num_strs;
-	char	*str_leng;
+	size_t	*str_leng;
 
 	i = 0;
 	num_strs = ft_count_total_strs(s, c);
-	str_leng = malloc(sizeof(char) * (num_strs + 1));
+	str_leng = malloc(sizeof(size_t) * (num_strs));
 	if (!(str_leng))
 		return (NULL);
-	str_leng[i] = '0';
+	while (i < num_strs)
+	{
+		str_leng[i] = 0;
+		i++;
+	}
+	i = 0;
 	while (*s)
 	{
 		if (*s != c)
-			str_leng[i]++;
-		if (*s == c)
-		{
+			str_leng[i] += 1;
+		else if (i < num_strs - 1)
 			i++;
-			str_leng[i] = '0';
-		}
 		s++;
 	}
-	str_leng[i + 1] = '\0';
 	return (str_leng);
 }
 
 char	**ft_split(char const *s, char c)
 {
-	char	*lengthof_eachstr;//how long is each str, needed for malloc
+	size_t	*lengthof_eachstr;//how long is each str, needed for malloc
 	size_t	n;//count strs
 	size_t	m;//count each chars
 	char	**strs_of_strs;//this is the RETURN VALUE
-	
+
 	lengthof_eachstr = ft_showlength_eachstr(s, c);
 	strs_of_strs = malloc(sizeof(char *) * (ft_count_total_strs(s, c) + 1));
-	//we only need to allocate mem for the start of each substr here.
 	if (!(strs_of_strs))
 		return (NULL);
 	n = 0;
@@ -76,19 +76,22 @@ char	**ft_split(char const *s, char c)
 		s++;
 	while (*s)
 	{
-		strs_of_strs[n] = malloc(sizeof(char) * (lengthof_eachstr[n] - '0' + 1));
+		strs_of_strs[n] = malloc(sizeof(char) * (lengthof_eachstr[n] + 1));
 		if (!(strs_of_strs[n]))
 			return (NULL);
-		m = 0;//for each str, the char counts start from zero
+		m = 0;
 		while (*s != c)
 		{
 			strs_of_strs[n][m] = *s;
 			m++;
-			s++;//t for general char counts incredent every time
+			s++;
 		}
 		strs_of_strs[n][m] = '\0';
+		while (*s == c)
+			s++;
 		n++;
+		s++;
 	}
-	strs_of_strs[n] = (char *)'\0';
+	strs_of_strs[n] = NULL;
 	return (strs_of_strs);
 }
--- ft_split_main.c
+++ ft_split_main.c
@@ -16,25 +16,34 @@
 #include <stdlib.h>
 
 size_t	ft_count_total_strs(char const *s, char c);
-char	*ft_showlength_eachstr(char const *s, char c);
+size_t	*ft_showlength_eachstr(char const *s, char c);
 char	**ft_split(char const *s, char c);
 
 int	main(void)
 {
 	size_t		count_strs;
-	char		*lengthof_eachstr;
+	size_t		*lengthof_eachstr;
 	char		**strs_of_strs;
-
+	size_t		n;
+	
 	count_strs = ft_count_total_strs("aa&bbb&ddddd", '&');
 	lengthof_eachstr = ft_showlength_eachstr("aa&bbb&ddddd", '&');
 	strs_of_strs = ft_split("aa&bbb&ddddd", '&');
-	printf("testing_process\n");
+	printf("print_number_of_substrs\n");
 	printf("%zu\n", count_strs);
-	printf("%s\n", lengthof_eachstr);
-	while (*strs_of_strs)
+	printf("print_length_of_substrs\n");
+	n = 0;
+	while (n < count_strs)
 	{
-		printf("%s\n", *strs_of_strs);
-		strs_of_strs++;
+		printf("%zu\n", lengthof_eachstr[n]);
+		n++;
 	}
+	printf("print_substrs\n");
+	n = 0;
+	while (strs_of_strs[n])
+    	{
+        	printf("%s\n", strs_of_strs[n]);
+        	n++;
+    	}
 	return (0);
 }
